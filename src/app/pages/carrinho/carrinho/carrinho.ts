e501f27cff9d68267d805ac9ed8a32a9e13d0da8
Cadu <cadumalmeida13@gmail.com>
2026-01-26 13:16:27 -0300

    agora mostra o total de preço do carrinho

--- src/app/pages/carrinho/carrinho/carrinho.ts
+++ src/app/pages/carrinho/carrinho/carrinho.ts
@@ -2,27 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarrinhoService } from '../../../service/carrinho/carrinho.service';
 import { ProdutoProps } from '../../../Produto.model';
+import { ButtonColor } from '../../../components/button-color/button-color';
 
 @Component({
   selector: 'app-carrinho',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonColor],
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.scss',
 })
 export class Carrinho {
   itensNoCarrinho: ProdutoProps[] = [];
-  constructor(private carrinhoService: CarrinhoService) {}
+  constructor(private carrinhoService: CarrinhoService) {
+    this.calcularTotal() // pra começar assim que o componente abri ja que a logica ta aqui
+  }
+  
+  ValorTotal: number = 0;
+  calcularTotal(){
+    this.ValorTotal = 0
+      for(let i = 0; i < this.itensNoCarrinho.length; i++){
+          this.ValorTotal += this.itensNoCarrinho[i].price!;
+        }
+        this.ValorTotal = Number(this.ValorTotal.toFixed(2));
+    }
 
   ngOnInit() {
     this.itensNoCarrinho = this.carrinhoService.obterItens();
+    this.calcularTotal()
   }
 
   atualizarLista() {
     this.itensNoCarrinho = this.carrinhoService.obterItens();
+    this.calcularTotal()
   }
 
   remover(id: number) {
-    this.carrinhoService.removerItem(id); // Remove no Service
-    this.atualizarLista(); // Atualiza o que aparece na tela
+    this.carrinhoService.removerItem(id);
+    this.atualizarLista(); 
+    this.calcularTotal()
   }
 }
