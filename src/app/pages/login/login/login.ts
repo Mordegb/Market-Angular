54b01440c2c2888c263433eb11760cf048b7d02b
Cadu <cadumalmeida13@gmail.com>
2026-01-30 08:48:06 -0300

    sistema de pegar input ta funcionando

--- src/app/pages/login/login/login.ts
+++ src/app/pages/login/login/login.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-
+  UserEmail: string = '';
+  UserPassword: string = '';
+ fazerLogin() {
+    console.log('Email:', this.UserEmail);
+    console.log('Senha:', this.UserPassword);
+    alert(`nome: ${this.UserEmail} , e ${this.UserPassword}`)
+  }
 }
--- src/app/service/carrinho/carrinho.service.ts
+++ src/app/service/carrinho/carrinho.service.ts
@@ -1,29 +1,28 @@
 import { Injectable } from '@angular/core';
-import { ProdutoProps } from '../../Produto.model'; 
+import { ProdutoProps } from '../../Produto.model';
 
 @Injectable({
-  providedIn: 'root' 
+  providedIn: 'root',
 })
 export class CarrinhoService {
   private itens: ProdutoProps[] = [];
 
-
   adicionarAoCarrinho(produto: ProdutoProps) {
     this.itens.push(produto);
-    console.log('Carrinho atual:', this.itens); 
+    console.log('Carrinho atual:', this.itens);
     alert(`produto adicionado ao carrinho!`);
   }
 
   obterItens() {
     return this.itens;
   }
-  
+
   limparCarrinho() {
     this.itens = [];
   }
 
   removerItem(id: number) {
-  this.itens = this.itens.filter(item => item.id !== id);
-  console.log('Item removido. Carrinho atual:', this.itens);
-}
+    this.itens = this.itens.filter((item) => item.id !== id);
+    console.log('Item removido. Carrinho atual:', this.itens);
+  }
 }
