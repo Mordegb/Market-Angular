c7c3efd02d8ea4a2a63ede98595f6f1cb927dd43
Cadu <cadumalmeida13@gmail.com>
2026-01-30 11:21:31 -0300

    funciona os formularios com validação de email

--- src/app/pages/login/login/login.ts
+++ src/app/pages/login/login/login.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,FormControl,FormGroup,Validators , ReactiveFormsModule} from '@angular/forms';
+import { form } from '@angular/forms/signals';
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule , ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-  UserEmail: string = '';
-  UserPassword: string = '';
+
+
+ loginForm = new FormGroup({ //vai criar as coisas do grupo que tem que validar
+  UserEmail: new FormControl('',[Validators.required , Validators.email]),
+  UserPassword: new FormControl('',[Validators.required])
+ });
+
  fazerLogin() {
-    console.log('Email:', this.UserEmail);
-    console.log('Senha:', this.UserPassword);
-    alert(`nome: ${this.UserEmail} , e ${this.UserPassword}`)
+   if(this.loginForm.valid){//verifica se as informações estão valido
+    console.log(this.loginForm.value)
+    alert(this.loginForm.value)
+   }
+   else{
+    alert("preencha os campos corretamente")
+   }
   }
 }
