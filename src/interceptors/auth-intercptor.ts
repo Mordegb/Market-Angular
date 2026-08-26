1eb62c0b2c2084c5e04a7299fc05b5f29ad1ea2d
Cadu Almeida <eduardo.allmeida113@gmail.com>
2026-08-26 16:28:36 -0300

    style: coments in functions

--- src/interceptors/auth-intercptor.ts
+++ src/interceptors/auth-intercptor.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
-import { AuthService } from '../app/service/auth/auth.service'; 
+import { AuthService } from '../app/service/auth/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
@@ -14,6 +14,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
       : req;
 
+  // em teoria me retornaria pro login se o token não valesse,mas o dummy nunca retorna 401
   return next(authReq).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
